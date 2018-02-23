ee327273156f2eea6b9877bc01ccb282d76dfa9a
bucricket <bucricket@gmail.com>
2018-02-23 12:30:30 -0500

    Dont add year

--- preprocess/processData.py
+++ preprocess/processData.py
@@ -635,10 +635,10 @@ class MET:
             reparsed = minidom.parseString(rough_string) #mini as minidom
             return '\n'.join([line for line in reparsed.toprettyxml(indent=' '*2).split('\n') if line.strip()]) 
 #====convert NETCDF data to VRTs===============================================
-        gsip_path = os.path.join(self.gsip_path, "%d" % self.year) 
-        if not os.path.exists(gsip_path):
-            os.mkdir(gsip_path)
-        gsip_fn = glob.glob(os.path.join(gsip_path,'*gsipL2_met10_MSGFD_%s_%02d30.nc.gz' % (date,self.hr)))[0]
+#        gsip_path = os.path.join(self.gsip_path, "%d" % self.year) 
+#        if not os.path.exists(gsip_path):
+#            os.mkdir(gsip_path)
+        gsip_fn = glob.glob(os.path.join(self.gsip_path,'*gsipL2_met10_MSGFD_%s_%02d30.nc.gz' % (date,self.hr)))[0]
         layers = ["flux_swd_sfc","latitude-pc","longitude-pc"]
         if os.path.exists(gsip_fn):
             gunzip(gsip_fn)
@@ -663,10 +663,10 @@ class MET:
                 outds = None
         #=======Daily GSIP====================================================      
 #        gsip_fn = glob.glob(os.path.join(self.gsip_path,'*.gsipL3_met10_%s_%d30.nc.gz' % (date,self.hr)))[0]
-        gsip_path = os.path.join(self.gsip_path, "%d" % self.year) 
-        if not os.path.exists(gsip_path):
-            os.mkdir(gsip_path)
-        gsip_fn = glob.glob(os.path.join(gsip_path,'*gsipL3_global_GDA_%s.nc.gz' % date))[0]
+#        gsip_path = os.path.join(self.gsip_path, "%d" % self.year) 
+#        if not os.path.exists(gsip_path):
+#            os.mkdir(gsip_path)
+        gsip_fn = glob.glob(os.path.join(self.gsip_path,'*gsipL3_global_GDA_%s.nc.gz' % date))[0]
         layers = ["insolation"]
         if os.path.exists(gsip_fn):
             gunzip(gsip_fn)
