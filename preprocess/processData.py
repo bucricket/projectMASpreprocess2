88e3662df881f93986a01e1f1500389872bcb459
bucricket <bucricket@gmail.com>
2018-02-23 12:36:51 -0500

    Added print statement

--- preprocess/processData.py
+++ preprocess/processData.py
@@ -638,7 +638,9 @@ class MET:
 #        gsip_path = os.path.join(self.gsip_path, "%d" % self.year) 
 #        if not os.path.exists(gsip_path):
 #            os.mkdir(gsip_path)
-        gsip_fn = glob.glob(os.path.join(self.gsip_path,'*gsipL2_met10_MSGFD_%s_%02d30.nc.gz' % (date,self.hr)))[0]
+        gsip_path = os.path.join(self.gsip_path,'*gsipL2_met10_MSGFD_%s_%02d30.nc.gz' % (date,self.hr))
+        print(gsip_path)
+        gsip_fn = glob.glob(gsip_path)[0]
         layers = ["flux_swd_sfc","latitude-pc","longitude-pc"]
         if os.path.exists(gsip_fn):
             gunzip(gsip_fn)
